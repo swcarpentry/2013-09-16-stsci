1c105b5ec24fa3dfb501e3ee398a5476e35e199e
Erik M. Bray <embray@stsci.edu>
2014-11-06 11:02:23 -0500

    Added scripts and setup.py directories; apparently I never actually committed this at the end of the tutorial.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name='myls',
+    version='0.1',
+    author='Erik Bray',
+    packages=['myls'],
+    scripts=['scripts/myls']
+)
